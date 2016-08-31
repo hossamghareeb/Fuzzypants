ca105c9c8d5abd82ebade64d743bf912c86033cf
Hossam Ghareeb <hossam.ghareb@gmail.com>
2016-09-01 00:25:37 +0400

    - Create the project :) - Adding data models

---
+++ Lifesum/Lifesum/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Lifesum
+//
+//  Created by Hossam Ghareeb on 8/31/16.
+//  Copyright © 2016 Hossam Ghareeb. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
